d4823373ea8b37376058f710d623c74b707d8a78
AthanasiosPapazoglou <athanasiospap95@gmail.com>
2021-09-16 19:05:57 +0300

    102-Deriving Recent Transactions

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -22,10 +22,10 @@ class Chart extends StatelessWidget {
         }
       }
 
-      print(DateFormat.E(weekDay));
+      print(DateFormat.E().format(weekDay));
       print(totalSum);
 
-      return {'day': DateFormat.E(weekDay), 'amount': totalSum};
+      return {'day': DateFormat.E().format(weekDay), 'amount': totalSum};
     });
   }
 
