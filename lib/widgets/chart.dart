90e5561789815180eb01971bcfec8690336456ca
AthanasiosPapazoglou <athanasiospap95@gmail.com>
2021-09-17 13:07:12 +0300

    108-ReOrdering Bars

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -27,7 +27,7 @@ class Chart extends StatelessWidget {
       print(totalSum);
 
       return {'day': DateFormat.E().format(weekDay), 'amount': totalSum};
-    });
+    }).reversed.toList();
   }
 
   double get totalSpending {
@@ -44,18 +44,20 @@ class Chart extends StatelessWidget {
       child: Padding(
         padding: EdgeInsets.all(10),
         child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: groupedTransactionValues.map((data) {
-          return Flexible(
-            fit: FlexFit.tight,
-            child: ChartBar(
-              data['day'], 
-              data['amount'],
-              totalSpending == 0.0 ? 0.0 : (data['amount'] as double) / totalSpending,
-             ),
-          );
-        }).toList(),
-      ),
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: groupedTransactionValues.map((data) {
+            return Flexible(
+              fit: FlexFit.tight,
+              child: ChartBar(
+                data['day'],
+                data['amount'],
+                totalSpending == 0.0
+                    ? 0.0
+                    : (data['amount'] as double) / totalSpending,
+              ),
+            );
+          }).toList(),
+        ),
       ),
     );
   }
